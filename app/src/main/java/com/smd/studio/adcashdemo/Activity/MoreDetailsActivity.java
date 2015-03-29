222be4c0ccbc4c4dae7022d1e689efe352b3cd63
Stefan Doychev <sdoychev@gmail.com>
2015-03-30 02:49:47 +0300

    Rock on.

--- app/src/main/java/com/smd/studio/adcashdemo/Activity/MoreDetailsActivity.java
+++ app/src/main/java/com/smd/studio/adcashdemo/Activity/MoreDetailsActivity.java
@@ -1,12 +1,9 @@
 package com.smd.studio.adcashdemo.Activity;
 
 import android.content.Intent;
-import android.graphics.Matrix;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.MotionEvent;
-import android.view.ScaleGestureDetector;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -18,9 +15,6 @@ public class MoreDetailsActivity extends ActionBarActivity {
 
     private String title, url, imgUrl;
     private ImageView image;
-    private Matrix matrix = new Matrix();
-    private float scale = 3f;
-    private ScaleGestureDetector SGD;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,24 +40,5 @@ public class MoreDetailsActivity extends ActionBarActivity {
                 startActivity(intent);
             }
         });
-        SGD = new ScaleGestureDetector(this, new ScaleListener());
-    }
-
-
-    @Override
-    public boolean onTouchEvent(MotionEvent ev) {
-        SGD.onTouchEvent(ev);
-        return true;
-    }
-
-    private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
-        @Override
-        public boolean onScale(ScaleGestureDetector detector) {
-            scale *= detector.getScaleFactor();
-            scale = Math.max(0.1f, Math.min(scale, 5.0f));
-            matrix.setScale(scale, scale);
-            image.setImageMatrix(matrix);
-            return true;
-        }
     }
 }
